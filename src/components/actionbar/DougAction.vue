8dbf0e623543eb0d6ef5ccc920fafa1a2ada18f4
dev-mbr <mbr.douglasmatos@gmail.com>
2020-04-22 15:33:34 -0300

    ajustes reunião 1 - meio

--- src/components/actionbar/DougAction.vue
+++ src/components/actionbar/DougAction.vue
@@ -30,18 +30,21 @@ export default {
 
 
 <style>
-  .footerbar {
-    position: fixed;
-    padding: 26px;
-    right: 0;
-    bottom: 0;
-    width: calc(100% - 15.2vw);
-    color: #494949;
-    background-color: #ffffff;
-    text-align: center;
-    border-top: 2px solid #e5e5e5;
+  @media (min-width: 1100px){
+    .footerbar {
+      position: fixed;
+      padding: 26px;
+      right: 0;
+      bottom: 0;
+      left: 260px;
+      color: #494949;
+      background-color: #ffffff;
+      text-align: center;
+      border-top: 2px solid #e5e5e5;
+    }
+    .seta-footer {
+      position: absolute; left: -104px; top:30%
+    }
   }
-  .seta-footer {
-    position: absolute; left: -104px; top:30%
-  }
+
 </style>
