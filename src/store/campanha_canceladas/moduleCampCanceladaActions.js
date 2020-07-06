37d94b1e9db431a2fa71f82d5c4e58a04aa1e980
dev-mbr <mbr.douglasmatos@gmail.com>
2020-07-06 15:17:20 -0300

    companhacanceladosContatos

--- src/store/campanha_canceladas/moduleCampCanceladaActions.js
+++ src/store/campanha_canceladas/moduleCampCanceladaActions.js
@@ -60,4 +60,13 @@ export default {
                 })
         })
     },
+    getContatos({commit}, dados) {
+      return new Promise((resolve, reject) => {
+        axios.get(`/api/campanha_cancelados_contatos`, {params: dados.params})
+          .then((response) => {
+            console.log('contatos resgatado', response);
+            resolve(response.data.data)
+          })
+      });
+    }
 }
