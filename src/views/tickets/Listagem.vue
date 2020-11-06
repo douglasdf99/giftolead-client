26b63c8d2caae68b2d19de5f71836d7d1b87d40c
dev-mbr <H4ck3r5215>
2020-11-06 11:05:13 -0300

    Correções no módulo de atendimento e listagem de Ticket.

--- src/views/tickets/Listagem.vue
+++ src/views/tickets/Listagem.vue
@@ -16,9 +16,9 @@
                 <vs-icon icon-pack="material-icons" icon="fiber_manual_record"
                          class="icon-grande text-success" v-if="ticket.status == 0"></vs-icon>
                 <vs-icon icon-pack="material-icons" icon="fiber_manual_record"
-                         class="icon-grande text-black" v-if="ticket.status == 2"></vs-icon>
-                <vs-icon icon-pack="material-icons" icon="fiber_manual_record"
                          class="icon-grande text-warning" v-if="ticket.status == 1"></vs-icon>
+                <vs-icon icon-pack="material-icons" icon="fiber_manual_record"
+                         class="icon-grande text-black" v-if="ticket.status == 2"></vs-icon>
                 <vs-icon icon-pack="material-icons" icon="fiber_manual_record"
                          class="icon-grande text-blue" v-if="ticket.status == 3"></vs-icon>
             </div>
@@ -27,7 +27,7 @@
                 <i class="material-icons hover:text-primary cursor-pointer" v-if="$acl.check('ticket_detalhar')" @click="$emit('detalhar', ticket.id)">visibility</i>
                 <i class="material-icons hover:text-primary cursor-pointer" v-if="canEditar(ticket)" @click="$emit('update', ticket)">edit</i>
                 <i class="material-icons hover:text-primary cursor-pointer" v-if="canDeletar(ticket)" @click="$emit('delete', ticket.id)">delete</i>
-                <!--<i class="material-icons hover:text-primary cursor-pointer" v-if="(ticket.status == 1 && $acl.check('ticket_transferir'))" @click="$emit('transfer', ticket.id)">forward</i>-->
+                <i class="material-icons hover:text-primary cursor-pointer" v-if="(ticket.status == 1 && $acl.check('ticket_transferir'))" @click="$emit('transfer', ticket.id)">forward</i>
             </div>
         </div>
     </div>
